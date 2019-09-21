e97be8d707580d5206d2991ad2f45c60f74494fe
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-09-21 11:13:17 +0200

    Create a project skeleton with Maven configuration file an initial Java source directory. The Maven artifact ID is "org.kortforsyningen:proj" and the Java package name is "org.kortforsyningen.proj". Those identifiers may be changed before project completion depending on feedback.

---
+++ src/main/java/org/kortforsyningen/proj/package-info.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2019 Agency for Data Supply and Efficiency
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Java Native Interface for the <a href="https://proj.org/">PROJ</a> C/C++ library.
+ * PROJ is a generic coordinate transformation software that transforms geospatial
+ * coordinates from one coordinate reference system (CRS) to another.
+ * This includes cartographic projections as well as geodetic transformations.
+ * This package exposes PROJ services as implementations of <a href="http://www.geoapi.org">GeoAPI</a> interfaces.
+ * Both PROJ and GeoAPI are modeled according the ISO 19111 international standard.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @version 1.0
+ * @since   1.0
+ */
+package org.kortforsyningen.proj;
