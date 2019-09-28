ac472c5ceb7035e75680c7e81dbd667d4e8521cd
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-09-28 12:55:40 +0200

    Provide a mechanism for wrapping shared pointers in a memory block that we can reference from a Java object. We can not (at this time) store a shared pointer directly in a Java object because we need a 64 bits pointer, while shared pointers are bigger (128 bits in our test). Allocation yet another memory block from JNI wrappers is a workaround, but it creates one more indirection level. We may remove this indirection level in a future version if we find a reliable way to store a shared pointer directly in some fields of a Java object.

--- src/main/cpp/bindings.cpp
+++ src/main/cpp/bindings.cpp
@@ -30,35 +30,35 @@
 using osgeo::proj::io::DatabaseContext;
 using osgeo::proj::io::DatabaseContextNNPtr;
 using osgeo::proj::io::AuthorityFactory;
-using osgeo::proj::io::AuthorityFactoryNNPtr;
+using osgeo::proj::io::AuthorityFactoryPtr;
 
 
 
 
 // ┌────────────────────────────────────────────────────────────────────────────────────────────┐
-// │                           HELPER METHODS (not invoked from Java)                           │
+// │                          HELPER FUNCTIONS (not invoked from Java)                          │
 // └────────────────────────────────────────────────────────────────────────────────────────────┘
 
 /*
  * NOTE ON CHARACTER ENCODING: this implementation assumes that the PROJ library expects strings
  * encoded in UTF-8, regardless the platform encoding. Consequently we use the JNI "StringUTF"
- * methods directly. It is not completely appropriate because JNI methods use a modified UTF-8,
+ * functions directly. It is not completely appropriate because JNI functions use a modified UTF-8,
  * but it should be okay if the strings do not use the null character (0) or the supplementary
  * characters (the ones encoded on 4 bytes in a UTF-8 string).
  */
 
 /**
  * Sends the given text to java.lang.System.Logger for the PROJ package.
- * We use this method for debugging purposes only. The use of Java logger
+ * We use this function for debugging purposes only. Use of Java logger
  * instead of C++ std::cout is for avoiding conflicts caused by different
  * languages writing to the same standard output stream.
  *
- * If this method can not print, than it silently ignores the given text.
+ * If this function can not print, than it silently ignores the given text.
  * Such failure could happen if the Java method has moved but this C++
- * code has not been updated accordingly.
+ * function has not been updated accordingly.
  *
  * @param  env     The JNI environment.
- * @param  caller  The class from which this method has been invoked.
+ * @param  caller  The class from which this function has been invoked.
  */
 void log(JNIEnv *env, const std::string &text) {
     jclass c = env->FindClass("org/kortforsyningen/proj/ObjectReference");
@@ -76,7 +76,7 @@ void log(JNIEnv *env, const std::string &text) {
 
 /**
  * Casts the given address as a pointer to PJ_CONTEXT.
- * This method is defined for type safety.
+ * This function is defined for type safety.
  *
  * @param  ctxPtr  The address of the PJ_CONTEXT for the current thread.
  * @return The given ctxPtr as a pointer to PJ_CONTEXT.
@@ -85,6 +85,63 @@ inline PJ_CONTEXT* as_context(jlong ctxPtr) {
     return reinterpret_cast<PJ_CONTEXT*>(ctxPtr);
 }
 
+
+/**
+ * Wraps the given shared pointer in a memory block that can be referenced from a Java object.
+ * We can not store shared pointer directly in Java object because we need a 64 bits pointer,
+ * while shared objects are bigger (128 bits in our tests). This function copies the shared pointer
+ * in a newly allocated block and returns the memory address of that block. We may revisit this
+ * strategy in a future version if we find a way to store directly shared pointer in a Java object
+ * without this indirection level.
+ *
+ * After return from this function, object.use_count() while have been increased by one, unless the
+ * application run out of memory in which case the use count is unchanged and this function returns 0.
+ *
+ * @param  object  The object to wrap in a memory block that can be referenced from a Java object.
+ * @return Address to store in the Java object, or 0 if out of memory.
+ */
+template <class T> jlong wrap_shared_ptr(std::shared_ptr<T> object) {
+    /*
+     * If the given object was newly allocated by the caller, then object.use_count() == 2 at this point
+     * (one reference hold by the caller and one reference hold in this function). The memory block below
+     * shall be initialized with zeros since this value may be read by the = operator.
+     */
+    std::shared_ptr<T> *wrapper = reinterpret_cast<std::shared_ptr<T>*>(calloc(1, sizeof(object)));
+    if (wrapper) {
+        *wrapper = object;          // This assignation also increases object.use_count() by one.
+    }
+    static_assert(sizeof(wrapper) <= sizeof(jlong), "Can not store pointer in a jlong.");
+    return reinterpret_cast<jlong>(wrapper);
+}
+
+
+/**
+ * Returns the shared pointer stored in the memory block at the given address.
+ * This function does not release the memory block. It can be invoked from C++
+ * code like below:
+ *
+ *   AuthorityFactoryPtr factory = unwrap_shared_ptr<AuthorityFactory>(ptr);
+ *
+ * @param  ptr  Address returned by wrap_shared_ptr(…). Shall not be zero.
+ * @return Shared pointer which was wrapped in the specified memory block.
+ */
+template <class T> std::shared_ptr<T> unwrap_shared_ptr(jlong ptr) {
+    std::shared_ptr<T> *wrapper = reinterpret_cast<std::shared_ptr<T>*>(ptr);
+    return *wrapper;
+}
+
+
+/**
+ * Frees the memory block wrapping the shared pointer.
+ * The use count of that shared pointer is decreased by one.
+ *
+ * @param  ptr  Address returned by wrap_shared_ptr(…). Shall not be zero.
+ */
+template <class T> void release_shared_ptr(jlong ptr) {
+    std::shared_ptr<T> *wrapper = reinterpret_cast<std::shared_ptr<T>*>(ptr);
+    *wrapper = nullptr;     // This assignation decreases object.use_count().
+    free(wrapper);
+}
 
 
 
@@ -98,7 +155,7 @@ inline PJ_CONTEXT* as_context(jlong ctxPtr) {
  * Each thread should have its own PJ_CONTEXT instance.
  *
  * @param  env     The JNI environment.
- * @param  caller  The class from which this method has been invoked.
+ * @param  caller  The class from which this function has been invoked.
  * @return The address of the new PJ_CONTEXT structure, or 0 in case of failure.
  */
 JNIEXPORT jlong JNICALL Java_org_kortforsyningen_proj_Context_create(JNIEnv *env, jclass caller) {
@@ -112,7 +169,7 @@ JNIEXPORT jlong JNICALL Java_org_kortforsyningen_proj_Context_create(JNIEnv *env
  * Releases a PJ_CONTEXT.
  *
  * @param  env     The JNI environment.
- * @param  caller  The class from which this method has been invoked.
+ * @param  caller  The class from which this function has been invoked.
  * @param  ctxPtr  The address of the PJ_CONTEXT to release.
  */
 JNIEXPORT void JNICALL Java_org_kortforsyningen_proj_Context_destroy(JNIEnv *env, jclass caller, jlong ctxPtr) {
@@ -131,7 +188,7 @@ JNIEXPORT void JNICALL Java_org_kortforsyningen_proj_Context_destroy(JNIEnv *env
  * Returns the PROJ release number.
  *
  * @param  env     The JNI environment.
- * @param  caller  The class from which this method has been invoked.
+ * @param  caller  The class from which this function has been invoked.
  * @return The PROJ release number, or NULL.
  */
 JNIEXPORT jstring JNICALL Java_org_kortforsyningen_proj_ObjectReference_version(JNIEnv *env, jclass caller) {
@@ -152,7 +209,7 @@ JNIEXPORT jstring JNICALL Java_org_kortforsyningen_proj_ObjectReference_version(
  * The factory should be used by only one thread at a time.
  *
  * @param  env        The JNI environment.
- * @param  caller     The class from which this method has been invoked.
+ * @param  caller     The class from which this function has been invoked.
  * @param  ctxPtr     The address of the PJ_CONTEXT for the current thread.
  * @param  authority  Name of the authority for which to create the factory.
  * @return The address of the new authority factory, or 0 in case of failure.
@@ -162,8 +219,8 @@ JNIEXPORT jlong JNICALL Java_org_kortforsyningen_proj_AuthorityFactory_newInstan
     if (authority_utf) {
         try {
             DatabaseContextNNPtr db = DatabaseContext::create(std::string(), std::vector<std::string>(), as_context(ctxPtr));
-            AuthorityFactoryNNPtr factory = AuthorityFactory::create(db, authority_utf);
-            // TODO: wrap the shared pointer.
+            AuthorityFactoryPtr factory = AuthorityFactory::create(db, authority_utf).as_nullable();
+            return wrap_shared_ptr(factory);
         } catch (const std::exception &e) {
             jclass c = env->FindClass("org/opengis/util/FactoryException");
             if (c) env->ThrowNew(c, e.what());
