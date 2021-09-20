753a1b7ae5f85402d2e10778ca05b40c80a111a8
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2021-09-20 12:00:37 +0200

    Regenerate Javadoc in order to reflect the change of package names from "kortforsyningen" to "osgeo". There is no other change in the source files.

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.kortforsyningen.proj"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.osgeo.proj"}];updateSearchResults();
